387f8c38691280d3b572417ed530d9497c97cc2f
Ayose <ayosec@gmail.com>
2017-10-10 20:02:44 +0100

    Include header to set UTF-8 encoding

--- src/http.rs
+++ src/http.rs
@@ -1,18 +1,24 @@
 use comrak::{markdown_to_html, ComrakOptions};
 use std::fs::File;
 use std::io::{self, Read};
-use tiny_http::Request;
-use tiny_http::Response;
+use tiny_http::{Request, Response, Header};
+
+const HEADER: &str = "<DOCTYPE html>\n<meta charset=\"utf-8\">\n";
 
 pub fn handle_request(request: Request, source: &str) {
-    let html = match read_source(source) {
+    let mut html = match read_source(source) {
         Ok(c) => markdown_to_html(&c, &ComrakOptions::default()),
         Err(e) => format!("Can't read '{}': {:?}\n", source, e),
     };
 
+    html.insert_str(0, HEADER);
+
     println!("Sent {} bytes to {}", html.len(), request.remote_addr());
 
-    let result = request.respond(Response::from_data(html));
+    let response = Response::from_data(html)
+                            .with_header(Header::from_bytes("Content-Type", "text/html").unwrap());
+
+    let result = request.respond(response);
 
     if let Err(err) = result {
         eprintln!("respond failed: {:?}", err);
